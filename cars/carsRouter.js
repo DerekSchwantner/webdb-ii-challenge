3ba1f3872e4376e6bf1cdf56055568c55cb94ec4
Derek S <dschwan21@me.com>
2019-07-16 15:29:42 -0700

    all crud is done

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -15,16 +15,15 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    const { id } = req.params;
-    const car = await db("cars").where({ id });
+    const car = await db("cars").where({ id: req.params.id });
 
-    res.json(car);
+    res.status(200).json(car);
   } catch (err) {
     res.status(500).json({ message: "Failed to retrieve car" });
   }
 });
 
-router.post("/", async (req, res) => {
+router.post("/", validateCarInfo, async (req, res) => {
   try {
     const carData = req.body;
     const [id] = await db("cars").insert(carData);
@@ -36,5 +35,42 @@ router.post("/", async (req, res) => {
     res.status(500).json({ message: "Failed to store data" });
   }
 });
+
+router.put("/:id", validateCarInfo, async (req, res) => {
+  const id = req.params.id;
+  try {
+    const updatedCarData = req.body;
+    const carId = await db("cars").update(updatedCarData);
+    const newCarEntry = await db("cars").where({ id: req.params.id });
+
+    res.status(200).json(newCarEntry);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Failed to update car data" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedCar = await db("cars")
+      .where({ id: req.params.id })
+      .delete();
+    res.status(200).json({ message: "Car has been deleted" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to delete car data" });
+  }
+});
+
+//Middleware
+
+function validateCarInfo(req, res, next) {
+  const carInfo = req.body;
+  console.log("time to validate the car info");
+  if (!carInfo.VIN || !carInfo.make || !carInfo.model || !carInfo.mileage) {
+    res.status(400).json({ message: "missing car data" });
+  } else {
+    next();
+  }
+}
 
 module.exports = router;
